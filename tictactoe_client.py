78cb69d7b6e5dcce28b9e9feb02a72b6db66f460
suzanada <sada@ku.edu.tr>
2016-05-17 23:43:15 +0300

    v1.2 timeout functionality added

--- tictactoe_client.py
+++ tictactoe_client.py
@@ -1,38 +1,60 @@
 import xmlrpclib
+import sys
+import select
 
 p = xmlrpclib.ServerProxy("http://localhost:8000")
-p.setSessionID()
-print str(p.generateClientID())
-print p.getCounter()
-sessionID = p.getCounter()
 
-print p.startGame()
-print "Your client ID is: ", p.getCounter()
+sessionID = p.generateClientID()
+p.setSessionID(sessionID)
+print p.startGame(sessionID)
+print "Your client ID is: ",sessionID
+
 
 print p.printBoard(sessionID)
 while p.getGameStatus(sessionID) == str(0):
     try:
-        userInput = str(raw_input("Please write a number between 0-8"))
-        if p.checkValidMove(sessionID,userInput):
-            p.movePlayer(sessionID,userInput)
-            print "Your Move______________"
-            print p.printBoard(sessionID)
-            if p.checkWinner(sessionID):
-                uinput = str(raw_input(p.strGameStatus(sessionID)))
-                p.endOrRestartGame(sessionID,uinput)
-                print p.printBoard(sessionID)
-            else:
-                p.checkMove(sessionID)
-                print "Computer's Move______________"
+        print " You only have 15 seconds or you will lose !"
+        fi, se, th = select.select([sys.stdin], [], [], 15 )
+        if (fi):
+            userInput =str(sys.stdin.readline().strip())
+            if p.checkValidMove(sessionID,userInput):
+                p.movePlayer(sessionID,userInput)
+                print "Your Move______________"
                 print p.printBoard(sessionID)
                 if p.checkWinner(sessionID):
-                    uinput = str(raw_input(p.strGameStatus(sessionID)))
-                    p.endOrRestartGame(sessionID,uinput)
-                    print p.printBoard(sessionID)
+                    print p.strGameStatus(sessionID)
+                    fi, se, th = select.select([sys.stdin], [], [], 15 )
+                    if (fi):
+                        uinput =str(sys.stdin.readline().strip())
+                        p.endOrRestartGame(sessionID,uinput)
+                        print p.printBoard(sessionID)
+                    else:
+                        print "Your answer was late. If you want to play, please start again."
+                        break
+
                 else:
-                    continue
+                    p.checkMove(sessionID)
+                    print "Computer's Move______________"
+                    print p.printBoard(sessionID)
+                    if p.checkWinner(sessionID):
+                        print p.strGameStatus(sessionID)
+                        fi, se, th = select.select([sys.stdin], [], [], 15 )
+                        if (fi):
+                            uinput =str(sys.stdin.readline().strip())
+                            p.endOrRestartGame(sessionID,uinput)
+                            print p.printBoard(sessionID)
+                        else:
+                            print "Your answer was late. If you want to play, please start again."
+                            break
+                    else:
+                        continue
+            else:
+                print "Please try again invalid move"
         else:
-            print "Please try again invalid move"
+            print "YOU LOST! Your answer was late. If you want to play, please start again."
+            break
     except:
-        print "Invalid move"
+        print "Invalid move, this is not even an integer"
+
+
 
